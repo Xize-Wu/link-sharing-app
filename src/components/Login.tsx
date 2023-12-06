86b6c6ed1487fdbd64e1b1d6c7c9f7786002a6c0
Eregion <wuxize1995@gmail.com>
2023-12-05 21:12:46 -0500

    modualize login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,51 +1,55 @@
-import styled from "styled-components";
-import LoginCreateButton from "../styles/LoginCreateButton";
-import { useDatabase } from "../contexts/DatabaseContext";
 import { useState } from "react";
+import styled from "styled-components";
+
+import { useDatabase } from "../contexts/DatabaseContext";
+
+import InputField from "../ui/InputField";
+import LoginCreateButton from "../ui/LoginCreateButton";
 
 const StyledContainer = styled.div`
 padding: 2px;
 width: 40em;
 border: 1px solid black;`
 
+interface LoginState {
+    email: string;
+    password: string;
+}
+
 function Login() {
     const { loginFunction } = useDatabase()    
-    interface LoginState {
-        email: string;
-        password: string;
-    }
     
     const [login, setLogin] = useState<LoginState>({ email: "", password: "" });
     
+    function handleEmailChange (e:React.ChangeEvent<HTMLInputElement>){
+        setLogin({...login, email: e.target.value})
+    }
+
+    function handelPasswordChange (e:React.ChangeEvent<HTMLInputElement>){
+        setLogin({...login, password: e.target.value})
+    }
+
     const handleLoginClick = () => {
         loginFunction(login);
     };
 
     return (
         <StyledContainer>
-            <div>Login</div>
-            <div>Add your details below to get back into the app</div>
-            <div>
-                <div>Email Address</div>
-                <input
-                    type="text"
-                    value={login.email}
-                    placeholder="e.g. alex@email.com"
-                    onChange={(e) => setLogin({ ...login, email: e.target.value })}
-                />
-            </div>
-            <div>
-                <div>Password</div>
-                <input
-                    type="password"
-                    value={login.password}
-                    placeholder="Enter your password"
-                    onChange={(e) => setLogin({ ...login, password: e.target.value })}
-                />
-                <div>
-                    Don't have an account? Create account
-                </div>
-            </div>
+            <InputField 
+                label="Email Address"
+                name="email-address"
+                type="text"
+                value={login.email}
+                placeholder="e.g. alex@email.com"
+                onChange={handleEmailChange}/>
+            <InputField
+                label="Password"
+                name="password"
+                type="password"
+                value={login.password}
+                placeholder="Enter your password"
+                onChange={handelPasswordChange}
+            />
             {/* <LoginCreateButton bgColor="#633CFF" buttonText="Login" onClick={loginFunction}/> */}
             <button onClick={handleLoginClick}>Login</button>
         </StyledContainer>
