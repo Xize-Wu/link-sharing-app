95bacce07c1c5b41a3dee7a2e1287ea7eb40bb6a
Eregion <wuxize1995@gmail.com>
2023-12-05 21:10:55 -0500

    format app layout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,12 +1,22 @@
 import Login from "./Login";
 import CreateAccount from "./CreateAccount";
+import {styled, css} from 'styled-components'
 
+const StyledLayout = styled.div`
+background-color:red;
+`
 function AppLayout() {
     return (
+        <StyledLayout>
+        <header></header>
         <main>
             <Login />
             <CreateAccount />
         </main>
+        <footer>
+            This is a footer
+        </footer>
+        </StyledLayout>
     )
 }
 
