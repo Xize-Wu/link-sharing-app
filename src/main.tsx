3f9f55c3ba05c87e108c225c6a17b8b9d176f7af
Eregion <wuxize1995@gmail.com>
2023-12-06 22:30:02 -0500

    remove styling from main

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import styled from "styled-components";
-
 import App from './App.tsx'
-
-const StyledContainer = styled.div`
-background-color: #FAFAFA;
-height: 100%;
-width: 100%;
-`
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StyledContainer>
       <App />
-    </StyledContainer>
   </React.StrictMode>,
 )
