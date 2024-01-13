3a47902d70f938f3d410f48bda198c50a8524b50
Eregion <wuxize1995@gmail.com>
2024-01-12 23:59:24 -0500

    modualize input box component

---
+++ src/ui/InputBox.tsx
@@ -0,0 +1,42 @@
+export default function InputBox (){
+    return (
+        <div>
+        {label === "Email Address" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="none"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="#737373"
+              d="M14 3H2a.5.5 0 0 0-.5.5V12a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1V3.5A.5.5 0 0 0 14 3Zm-.5 9h-11V4.637l5.162 4.732a.5.5 0 0 0 .676 0L13.5 4.637V12Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="none"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="#737373"
+              d="M13 5h-2V3.5a3 3 0 0 0-6 0V5H3a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1ZM8.5 9.914V11.5a.5.5 0 0 1-1 0V9.914a1.5 1.5 0 1 1 1 0ZM10 5H6V3.5a2 2 0 1 1 4 0V5Z"
+            />
+          </svg>
+        )}
+        <input
+          name={name}
+          key={name}
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      </div>
+
+    )
+}
